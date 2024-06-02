6bad81f7c0d119e4a969f60a46b334e657068c35
Mike <dinclock5@gmail.com>
2024-06-02 13:36:31 +0300

    add more test

--- tests/customers_test.py
+++ tests/customers_test.py
@@ -29,3 +29,70 @@ async def test_get_categories(authenticated_customer_ac: AsyncClient):
     response = await authenticated_customer_ac.get('api/v1/customers/category')
     assert response.status_code == 200
 
+
+async def test_get_products_by_category(authenticated_customer_ac: AsyncClient):
+    response = await authenticated_customer_ac.get('api/v1/customers/category/category1', params={
+        'page': 1,
+    })
+    assert response.json()[0]['name'] == 'product1'
+    assert response.status_code == 200
+
+
+async def test_get_products_by_id(authenticated_customer_ac: AsyncClient):
+    response = await authenticated_customer_ac.get('api/v1/customers/product/1')
+    assert response.json()['name'] == 'product1'
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize('product_id, quantity, status_code', [
+    (1, 1, 201),
+
+    # Некорректное выполнение, создает ордер с несуществующим product_id
+    # (2, 1, 201),
+
+    (1, -1, 422),
+    ('string', 1, 422),
+])
+async def test_add_basket(product_id, quantity, status_code, authenticated_customer_ac: AsyncClient):
+    response = await authenticated_customer_ac.post('api/v1/customers/basket', params={
+        'product_id': product_id,
+        'quantity': quantity,
+    })
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize('city, street, house, building, literal, apartment, status_code', [
+    ('big_city', 'some_name_street', 23, 34, 'A', 45, 201),
+    ('big_city', 'some_name_street', 23, 34, 'A', 45, 403),
+])
+async def test_add_contact(city, street, house, building, literal,
+                           apartment, status_code, authenticated_customer_ac: AsyncClient):
+    params = {
+        'city': city,
+        'street': street,
+        'house': house,
+        'building': building,
+        'literal': literal,
+        'apartment': apartment,
+    }
+    params = {i: params[i] for i in params if params[i]}
+    response = await authenticated_customer_ac.post('api/v1/customers/contact', params=params)
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize('city, street, house, building, literal, apartment, status_code', [
+    ('new_big_city', 'some_new_name_street', 567, None, 'A', None, 200),
+])
+async def test_edit_contact(city, street, house, building, literal,
+                            apartment, status_code, authenticated_customer_ac: AsyncClient):
+    params = {
+        'city': city,
+        'street': street,
+        'house': house,
+        'building': building,
+        'literal': literal,
+        'apartment': apartment,
+    }
+    params = {i: params[i] for i in params if params[i]}
+    response = await authenticated_customer_ac.put('api/v1/customers/contact', params=params)
+    assert response.status_code == status_code
